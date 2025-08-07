d54de1e4146d10dae3259beefb5b82a0139d6f50
solbank1 <javisolisayali@gmail.com>
2025-08-07 11:32:34 -0300

    Create __manifest__.py

---
+++ solbank_op/solbank_op/solbank_op/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'SolBank Operaciones',
+    'version': '1.0',
+    'summary': 'Módulo para operaciones de giros, cambios, arbitraje y más',
+    'description': 'Módulo personalizado para SolBank: gestión de giros, cambios, arbitraje, transferencias y operaciones financieras.',
+    'author': 'Javi Solis',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
